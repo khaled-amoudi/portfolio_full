b26211d6a8f9dde469a332861684e5af42f9b497
Khaled Amoudi <67203109+khaled-amoudi@users.noreply.github.com>
2025-11-10 16:18:34 +0100

    add new skills

--- resources/views/frontend/partials/about_page.blade.php
+++ resources/views/frontend/partials/about_page.blade.php
@@ -269,20 +269,6 @@
                         <div class="card px-0 py-2 p-sm-2 bg-black-100 mt-2">
                             <div class="row">
                                 <div class="col-12 col-sm-4 d-flex justify-content-center justify-content-sm-start">
-                                    <img src="{{ asset('frontend/images/skills/docker.svg') }}" class="rounded"
-                                        width="50px" height="40px" alt="" />
-                                </div>
-                                <div
-                                    class="col-12 col-sm-8 mt-1 mt-sm-0 d-flex justify-content-center justify-content-sm-start align-items-center">
-                                    <span class="text-white text-center text-sm-start fs-12px fw-light letter-spacing-1px">Docker</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-4 col-sm-3 col-md-2 px-1">
-                        <div class="card px-0 py-2 p-sm-2 bg-black-100 mt-2">
-                            <div class="row">
-                                <div class="col-12 col-sm-4 d-flex justify-content-center justify-content-sm-start">
                                     <img src="{{ asset('frontend/images/skills/laravel.svg') }}" class="rounded"
                                         width="50px" height="40px" alt="" />
                                 </div>
@@ -373,6 +359,20 @@
                                 <div
                                     class="col-12 col-sm-8 mt-1 mt-sm-0 d-flex justify-content-center justify-content-sm-start align-items-center">
                                     <span class="text-white text-center text-sm-start fs-12px fw-light letter-spacing-1px">Git</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 col-sm-3 col-md-2 px-1">
+                        <div class="card px-0 py-2 p-sm-2 bg-black-100 mt-2">
+                            <div class="row">
+                                <div class="col-12 col-sm-4 d-flex justify-content-center justify-content-sm-start">
+                                    <img src="{{ asset('frontend/images/skills/Docker.svg') }}" class="rounded"
+                                        width="50px" height="40px" alt="" />
+                                </div>
+                                <div
+                                    class="col-12 col-sm-8 mt-1 mt-sm-0 d-flex justify-content-center justify-content-sm-start align-items-center">
+                                    <span class="text-white text-center text-sm-start fs-12px fw-light letter-spacing-1px">Docker</span>
                                 </div>
                             </div>
                         </div>
